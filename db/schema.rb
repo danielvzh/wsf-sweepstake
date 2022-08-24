a0fe25cf3822ac2bbb8f106aea752998804c7dc8
Daniel Zhukovsky <daniel.zhukovsky@gmail.com>
2022-08-24 01:54:56 +0100

    fixed member model

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_002757) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "points"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
