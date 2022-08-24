1865b9d2d39f4126d6f8da99260ac3b04d2b334e
Daniel Zhukovsky <daniel.zhukovsky@gmail.com>
2022-08-24 14:45:36 +0100

    members CRUD

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,43 @@
+class MembersController < ApplicationController
+  before_action :set_member, only: %I[show edit update destroy]
+
+  def index
+    @members = Member.all
+  end
+
+  def show
+  end
+
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.new(member_params)
+    @member.save
+    redirect_to member_path(@member)
+  end
+
+  def edit
+  end
+
+  def update
+    @member.update(member_params)
+    redirect_to member_path(@member)
+  end
+
+  def destroy
+    @member.destroy
+    redirect_to members_path, status: :see_other
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :points)
+  end
+
+  def set_member
+    @member = Member.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,24 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  # GET url/path
+  # # Read all
+  # get 'members', to: 'members#index'
+  # # Create
+  # get 'members/new', to: 'members#new'
+  # post 'members', to: 'members#create'
+  # # Read one
+  # get 'members/:id', to: 'members#show', as: :member
+  # # Update
+  # get 'members/:id/edit', to: 'members#edit'
+  # patch 'members/:id', to: 'members#update'
+  # # Delete
+  # delete 'members/:id', to: 'members#destroy'
 
+  # generate 7 routes above with just one line
+  resources :members
+
+  # GET url/about
   get 'about', to: 'pages#about'
-
+  # GET url/contact
   get 'contact', to: 'pages#contact'
 end
