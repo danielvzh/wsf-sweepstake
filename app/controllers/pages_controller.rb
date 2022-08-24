d9e07048021baa152e19b63a0d07f763141b8aed
Daniel Zhukovsky <daniel.zhukovsky@gmail.com>
2022-08-24 01:39:42 +0100

    Add memeber model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @members = %W(x y z a)
+
+    if params[:member]
+      @members = @members.select { |member| member.start_with? params[:member].downcase }
+    end
   end
 
   def about
