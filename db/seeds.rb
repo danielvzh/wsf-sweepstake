d267d0459e4de8cb5137219d9d519b5a90093431
Daniel Zhukovsky <daniel.zhukovsky@gmail.com>
2022-08-24 16:41:17 +0100

    seed 100 members

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database'
+Member.destroy_all
+
+puts 'Creating members'\
+
+100.times do
+  member = Member.create(
+    name: Faker::Name.first_name,
+    points: rand(1..10)
+  )
+  puts "Member with id: #{member.id} has been created"
+end
+puts 'Finished!'
